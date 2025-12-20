20f3d93ebb567032185309e63ca365449feb02ba
SKDondapati <skdondapati@stu.naperville203.org>
2025-12-20 14:01:35 -0600

    12/20/25

--- YK-Find the Exit/Assets/Scripts/PlayerMovment.cs
+++ YK-Find the Exit/Assets/Scripts/PlayerMovment.cs
@@ -7,13 +7,19 @@ public class PlayerMovment : MonoBehaviour
     public float speed = 5;
     void Start()
     {
-        //transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        GetComponent<Rigidbody>().velocity = Vector3.forward * speed;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        GetComponent<Rigidbody>().velocity = Vector3.forward * speed;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 destination = new Vector3(0, 0, horizontal);
+        GetComponent<Rigidbody>().velocity = destination * speed;
     }
 }
+
+
